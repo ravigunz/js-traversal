b2c642d0d75720695ec9f29b8ab6dfb1be3bb5bb
Ravi <ravigunz@gmail.com>
2023-03-01 00:16:01 +0530

    querySelector and querySelectorAll

---
+++ script.js
@@ -0,0 +1,28 @@
+// const grandparent = document.getElementById("grandparent-id");
+// changeColor(grandparent);
+// function changeColor(element){
+//     element.style.backgroundColor = "#001589"
+// }
+
+// const parents = document.querySelectorAll('.parent');
+// parents.forEach(changeColor)
+
+// changeColor(parent);
+// function changeColor(element){
+// element.style.backgroundColor = "#990866";
+// }
+
+
+// const children = document.querySelectorAll('.child');
+// children.forEach(changeColor)
+// changeColor(children);
+// function changeColor(element){
+//     element.style.backgroundColor = "#968539";
+// }
+
+const children = document.querySelectorAll('.child');
+children.forEach(changeColor)
+
+function changeColor(element){
+    element.style.backgroundColor = "#582345";
+}
